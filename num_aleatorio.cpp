aebf0ad2213cf0f8caf6571518618949d3a3747d
jesseniajustiniano <155786502+jesseniajustiniano@users.noreply.github.com>
2024-01-24 21:41:46 -0400

    Add files via upload

---
+++ num_aleatorio.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+int aleatorio(){
+	return rand() %100;
+}
+bool esImpar(int numero){
+ return numero %2!=0;
+}
+int main(int argc, char *argv[]) {
+	//En base a funciones imprimir N numeros impares. N=5
+	int n,c=0, num;
+	cout<<"generar N numero impares, ingrese n:";
+	cin>>n;
+	while(c<n)
+	{
+		num=aleatorio();
+		if(esImpar(num));
+		{
+			c++;
+			cout<<"impar["<<c<<"]:"<<num<<endl;
+		}
+	}
+
+return 0;
+}
